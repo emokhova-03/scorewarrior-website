60368dea8ac9ce0738d6b7ced3b2baa6184f5120
Elizaveta Mokhova <emokhova@scorewarrior.com>
2026-09-01 11:13:37 +0300

    feat(web): base layout with site header and footer

--- candidate-b-astro/astro.config.mjs
+++ candidate-b-astro/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://scorewarrior-website.emokhova.workers.dev/",
-    output: "static",
+  site: "https://scorewarrior-website.emokhova.workers.dev/",
+  output: "static",
 });
